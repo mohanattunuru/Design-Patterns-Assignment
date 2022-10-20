dcc9af6ac17975c44d6bcccc89a130a3e7851894
Mohana Narasimha Reddy Attunuru <mattunur@asu.edu>
2022-10-20 01:25:46 -0700

    login

--- Login.java
+++ Login.java
@@ -46,6 +46,86 @@ public class Login extends JFrame{
         p3.add(label1);
         p3.add(password);
         contentPane.add(p3);
+
+        buyerButton = new JRadioButton("Buyer    ");
+        sellerButton = new JRadioButton("Seller");
+        contentPane.add(buyerButton);
+        contentPane.add(sellerButton);
+
+        JButton loginButton = new JButton("Login");
+        JPanel p1 = new JPanel(new FlowLayout());
+        p1.add(loginButton);
+        contentPane.add(p1);
+
+        buyerButton.addActionListener(e -> sellerButton.setSelected(false));
+        sellerButton.addActionListener(e -> buyerButton.setSelected(false));
+       
+        loginButton.addActionListener(e->{
+            if(buyerButton.isSelected() && buyers.containsKey(username.getText())){
+                String val = buyers.get(username.getText());
+                if(val.equals(password.getText())){
+                    userType = 0;
+                    person = new Buyer(username.getText(), password.getText(), facade);
+                    login = true;
+                    counter = 1;
+                }
+            }else if(sellerButton.isSelected() && sellers.containsKey(username.getText())){
+                String val = sellers.get(username.getText());
+                if(val.equals(password.getText())){
+                    userType = 1;
+                    person = new Seller(username.getText(), password.getText(), facade);
+                    login = true;
+                    counter = 1;
+                }
+            }
+        });
+        setContentPane(contentPane);
+        requestFocus();
+        setFocusable(true);
+        pack();
+
+    }
+    private void getBuyers() {
+        buyers = new HashMap<>();
+        try {
+            Scanner sc = new Scanner(new File("BuyerInfo.txt"));
+            while(sc.hasNextLine()){
+                String str = sc.nextLine();
+                str = str.replace("\n", "");
+                String[] str1 = str.split(":");
+                buyers.put(str1[0], str1[1]);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
+    private void getsellers() {
+        sellers = new HashMap<>();
+        try {
+            Scanner sc = new Scanner(new File("SellerInfo.txt"));
+            while(sc.hasNextLine()){
+                String str = sc.nextLine();
+                str = str.replace("\n", "");
+                String[] str1 = str.split(":");
+                sellers.put(str1[0], str1[1]);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean getResult() {
+        while (counter == 0)
+            Thread.onSpinWait();
+        return login;
+    }
+
+    public int getUserType() {
+        return userType;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
 }
