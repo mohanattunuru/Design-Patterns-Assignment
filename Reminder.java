33a3922804e3a38bbe5c69a5b8f0de73b8dc7ce9
Mohana Narasimha Reddy Attunuru <mattunur@asu.edu>
2022-10-20 23:40:16 -0700

    remindervisitor

---
+++ Reminder.java
@@ -0,0 +1,17 @@
+
+
+public class Reminder {
+    private String Textremind;
+
+    public Reminder(String remind) {
+        Textremind = remind;
+    }
+
+    public String getReminder() {
+        return Textremind;
+    }
+
+    public void setReminder(String remind) {
+        Textremind = remind;
+    }
+}
---
+++ ReminderVisitor.java
@@ -0,0 +1,24 @@
+// Visitor pattern is used to send the reminder to user
+
+public class ReminderVisitor extends  NodeVisitor{
+    private Reminder reminder;
+
+    public ReminderVisitor(Reminder reminder, Facade facade, ClassProductList cls, Trading trade) {
+        this.reminder = reminder;
+    }
+
+    @Override
+    public void visitProduct(Product product) {
+        product.accept(this);
+    }
+
+    @Override
+    public void visitTrading(Trading trade) {
+        trade.accept(this);
+    }
+
+    @Override
+    public void visitFacade(Facade facade) {
+        facade.accept(this);
+    }
+}
